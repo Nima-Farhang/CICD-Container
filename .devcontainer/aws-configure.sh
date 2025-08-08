67b8ba669b30c9344dc8977aa3778e91377e07d7
Nima Farhang <nima@farhang.io>
2025-08-08 02:55:44 +0000

    aws cli configured

--- .devcontainer/aws-configure.sh
+++ .devcontainer/aws-configure.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e
 
-# Set AWS CLI SSO configuration for profile "Nima"
-aws configure set sso_start_url "https://d-9067e9a26d.awsapps.com/start" --profile Nima
-aws configure set sso_region "us-east-1" --profile Nima
-aws configure set sso_account_id "767397659305" --profile Nima
-aws configure set sso_role_name "AdministratorAccess" --profile Nima
-aws configure set region "ap-southeast-2" --profile Nima
-aws configure set output "json" --profile Nima
-aws configure set sso_session "Terraform" --profile Nima
-aws configure set sso_registration_scopes "sso:account:access" --profile Nima
+AWS_CONFIG_FILE="${HOME}/.aws/config"
+mkdir -p "$(dirname "$AWS_CONFIG_FILE")"
 
-# Create SSO session config
-aws configure set sso_session.Terraform.sso_start_url "https://d-9067e9a26d.awsapps.com/start"
-aws configure set sso_session.Terraform.sso_region "us-east-1"
-aws configure set sso_session.Terraform.sso_registration_scopes "sso:account:access"
+cat > "$AWS_CONFIG_FILE" <<EOF
+[profile Nima]
+sso_session = Terraform
+sso_account_id = 767397659305
+sso_role_name = AdministratorAccess
+region = ap-southeast-2
+output = json
 
-# Optionally, login (will require browser interaction)
-# aws sso login --profile Nima
+[sso-session Terraform]
+sso_start_url = https://d-9067e9a26d.awsapps.com/start
+sso_region = us-east-1
+sso_registration_scopes = sso:account:access
+EOF
 
-echo "AWS CLI SSO profile 'Nima' configured."
+echo "AWS CLI SSO profile 'Nima' and session 'Terraform' configured."

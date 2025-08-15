57e2138d38c3dd27e4d33eaf657cc67a76e4f019
Nima Farhang <nima@farhang.io>
2025-08-15 00:06:40 +0000

    changed backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
 # Late on you can use this configuration to set up an S3 backend for Terraform state management.
 # terraform {
 #   backend "s3" {
-#     bucket = "my-terraform-state"
+#     bucket = "terraform-state-bucket"
 #     key    = "${var.environment}/terraform.tfstate"
 #     region = "ap-southeast-2"
 #     dynamodb_table = "terraform-locks"
